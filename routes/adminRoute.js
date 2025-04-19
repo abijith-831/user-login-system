998dc00089dcc7d20496615850a25f25e20368c2
Abhijith <abhijithasokan831@gmail.com>
2025-04-19 19:36:56 +0530

    session part changed2

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,7 +3,11 @@ const admin_route = express();
 
 const session = require("express-session");
 const config = require("../config/config");
-admin_route.use(session({secret:config.sessionSecret}));
+admin_route.use(session({
+    secret: config.sessionSecret,
+    resave: false,             
+    saveUninitialized: false    
+}));
 
 const bodyParser = require("body-parser");
 admin_route.use(bodyParser.json());
