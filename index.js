ccd32daef491cb1c15df726ae31e3ddfe3113014
Abhijith <abhijithasokan831@gmail.com>
2025-04-19 20:03:58 +0530

    mongourl changedd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://username:password@cluster0-shard-00-00.os4ca.mongodb.net:27017,cluster0-shard-00-01.os4ca.mongodb.net:27017,cluster0-shard-00-02.os4ca.mongodb.net:27017/mydb?ssl=true&replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority");
+// mongoose.connect("mongodb://username:password@cluster0-shard-00-00.os4ca.mongodb.net:27017,cluster0-shard-00-01.os4ca.mongodb.net:27017,cluster0-shard-00-02.os4ca.mongodb.net:27017/mydb?ssl=true&replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority");
+mongoose.connect('mongodb+srv://abhijithasokan8311:sC8pvlOkWoKms7Iw@cluster0.7p337q0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 const express = require("express");
 const nocache = require('nocache');
 const app = express();
